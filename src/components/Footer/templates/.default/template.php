c26bb34efeef17bb789c6d6697cb0627d4ac9f2d
MukhametDev <zhanbekovm@mail.ru>
2024-07-26 12:35:04 +0500

    13683:Реализует компонент Footer
    Подставит динамические данные в компоненту Footer

--- src/components/Footer/templates/.default/template.php
+++ src/components/Footer/templates/.default/template.php
@@ -1,47 +1,39 @@
 <section class="secury">
-    <div class="secury__container">
-        <ul class="secury__list">
-            <li class="secury__item">2024 “Мебель.ру” Все права защищены.</li>
-            <li class="secury__item">Политика конфиденциальности</li>
-            <li class="secury__item">Разработка сайта интернет компания “SunWeb”</li>
-        </ul>
-    </div>
-    <footer class="footer footer_bg">
-	<div class="footer__container">
-		<div class="footer__content">
-			<div class="footer__logo footer__logo_1">
-				<p class="footer__title">Мебель</p>
-				<p class="footer__text">Центр мебельных технологий</p>
-			</div>
-			<div class="footer__items">
-				<div class="footer__item item-footer">
-					<h3 class="item-footer__title item-footer__title_nav">Меню</h3>
-					<ul class="item-footer__lists">
-						<li class="item-footer__item"><a href="#" class="item-footer__link item-footer__link_nav">Шкафы
-								купе</a></li>
-						<li class="item-footer__item"><a href="#"
-								class="item-footer__link item-footer__link_nav">Кухни</a></li>
-						<li class="item-footer__item"><a href="#"
-								class="item-footer__link item-footer__link_nav">Торговое оборудование</a>
-						</li>
-						<li class="item-footer__item"><a href="#" class="item-footer__link item-footer__link_nav">О
-								компании</a></li>
-						<li class="item-footer__item"><a href="#"
-								class="item-footer__link item-footer__link_nav">Контакты</a></li>
-					</ul>
-				</div>
-				<div class="footer__item item-footer item-footer_2">
-					<h3 class="item-footer__title">Контакты</h3>
-					<ul class="item-footer__lists">
-						<li class="item-footer__item"><a href="#" class="item-footer__link">info@mebel.ru</a></li>
-						<li class="item-footer__item"><a href="#" class="item-footer__link">г.Тюмень, ул. Калинина,
-								22/1</a>
-						</li>
-						<li class="item-footer__item"><a href="#" class="item-footer__link">+7 (3452) 00-00-00</a></li>
-					</ul>
-				</div>
-			</div>
-			<button class="footer__btn footer__btn_3">Авторизоваться</button>
-		</div>
+	<div class="secury__container">
+		<ul class="secury__list">
+			<?php foreach ($arParams['footer-items'] as $item) : ?>
+				<li class="secury__item"><?php echo htmlspecialchars($item['name']) ?></li>
+			<?php endforeach; ?>
+		</ul>
 	</div>
-</footer>
+	<footer class="footer footer_bg">
+		<div class="footer__container">
+			<div class="footer__content">
+				<div class="footer__logo footer__logo_1">
+					<p class="footer__title"><?php echo htmlspecialchars($arParams['logo']) ?></p>
+					<p class="footer__text"><?php echo htmlspecialchars($arParams['logo-text']) ?></p>
+				</div>
+				<div class="footer__items">
+					<div class="footer__item item-footer">
+						<h3 class="item-footer__title item-footer__title_nav"><?php echo htmlspecialchars($arParams['menu-title']) ?></h3>
+						<ul class="item-footer__lists">
+							<?php foreach ($arParams['menu'] as $item) : ?>
+								<li class="item-footer__item"><a href="<?php echo htmlspecialchars($item['href']) ?>" class="item-footer__link item-footer__link_nav"><?php echo htmlspecialchars($item['item']) ?></a>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+					<div class="footer__item item-footer item-footer_2">
+						<h3 class="item-footer__title"><?php echo htmlspecialchars($arParams['contact-title']) ?></h3>
+						<ul class="item-footer__lists">
+							<?php foreach ($arParams['contacts'] as $item) : ?>
+								<li class="item-footer__item"><a href="#" class="item-footer__link"><?php echo htmlspecialchars($item['item']) ?></a></li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+				</div>
+				<button class="footer__btn footer__btn_3"><?php echo htmlspecialchars($arParams['btnTitle']) ?></button>
+			</div>
+		</div>
+	</footer>
+</section>

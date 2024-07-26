522b12a09c36309177e994ccc6d7278b558384d9
MukhametDev <zhanbekovm@mail.ru>
2024-07-26 11:45:53 +0500

    13683:Реализует компоненту Switch
    Реализует динамическую подстановку данных в компоненту Switch

--- src/components/Switch/templates/.default/template.php
+++ src/components/Switch/templates/.default/template.php
@@ -6,8 +6,8 @@
                 <?php foreach ($arParams['cards'] as $item) : ?>
                     <div class="switch__card card-switch">
                         <img src="<?php echo htmlspecialchars($item['img']) ?>" alt="icon" class="card-switch__img">
-                        <h3 class="card-switch__title"><?php echo htmlspecialchars($item['i']) ?></h3>
-                        <p class="card-switch__text">Если у вас есть пожелания, изготовим кухню согласно вашим примерам</p>
+                        <h3 class="card-switch__title"><?php echo htmlspecialchars($item['title']) ?></h3>
+                        <p class="card-switch__text"><?php echo htmlspecialchars($item['text']) ?></p>
                     </div>
                 <?php endforeach; ?>
             </div>
